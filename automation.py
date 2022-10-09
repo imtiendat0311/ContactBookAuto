ea982ecfe3fdfff4cb704eb29867db2733d89599
superboo0311 <60676245+superboo0311@users.noreply.github.com>
2022-10-08 22:06:39 -0400

    prettier

--- automation.py
+++ automation.py
@@ -24,29 +24,20 @@ def autofill(d):
     URL="https://app.contractbook.com/drafter/Esoft/freelancer-contract-7139e0f0/sign-in" # biến chứa string
     browser.get(URL)
 
-    # time.sleep(5)
     #email field
     WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.XPATH,"/html/body/div[1]/div/main/div[1]/form/div/span/div/div/input"))).send_keys("scintern.vn@esoft.com")
-    # browser.find_element(By.XPATH,"/html/body/div[1]/div/main/div[1]/form/div/span/div/div/input").send_keys("scintern.vn@esoft.com")
-    # time.sleep(2)
-    WebDriverWait(browser,20).until(EC.element_to_be_clickable((By.XPATH,"/html/body/div[1]/div/main/div[1]/form/div/button"))).click()
     #button to sign in
-    # browser.find_element(By.XPATH,"/html/body/div[1]/div/main/div[1]/form/div/button").click()
-    # time.sleep(6)
+    WebDriverWait(browser,20).until(EC.element_to_be_clickable((By.XPATH,"/html/body/div[1]/div/main/div[1]/form/div/button"))).click()
     #get start button
     WebDriverWait(browser,20).until(EC.element_to_be_clickable((By.XPATH,"/html/body/div[6]/div/div[1]/div/div/div/button"))).click()
-    # browser.find_element(By.XPATH,"/html/body/div[6]/div/div[1]/div/div/div/button").click()  
-    # time.sleep(3)
+    
     d.insert(1,"Việt Nam") #country
     d.insert(3,"Vietnamese") #nationality 
     d.insert(4,"Asia") # region
 
-    # 2 hang manh , hoan kiem , ha noi 
-    # d.insert(4,d[2].split(", ")[-1]) #region chia address bằng dấu , rồi lấy phần cuối của address 
     listID = ['fullName','country','address','nationality','region','dateOfBirth',
     'passportOrNationalIdNumber','issuedDateAndUssuedPlaceOfPassportid',
     'accountNumber','bankName','bankAddress','swiftCode','email']
-    # browser.find_element(By.ID,"lm-accept-necessary").click()
     # loop đi từ 0 -> 11 bỏ qua email (cần dùng lệnh riêng)  
     for i in range(len(listID)-1):
         a=WebDriverWait(browser,20).until(EC.presence_of_element_located((By.ID,listID[i]))) # tìm ô cần điền 
@@ -67,22 +58,10 @@ def autofill(d):
     WebDriverWait(browser,20).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[1]/div/main/div/div/nav/div[1]/div/button'))).click()
     current_url = browser.current_url
     WebDriverWait(browser, 20).until(EC.url_changes(current_url))
-
-    # browser.find_element(By.XPATH,"/html/body/div[1]/div/main/div/div[2]/div/div/div/button").send_keys(Keys.ENTER)
-    # time.sleep(3)
-    # open contractbook button 
-    # browser.find_element(By.XPATH,'/html/body/div[1]/div/main/div/div/nav/div[1]/div/button').click()
-    # time.sleep(7) 
-    # browser.find_element(By.ID,'microsoft-button').click()
     browser.quit()
     stop = time.perf_counter()
     print(f"\n\nTake "+str(stop-start)+" s to finish fill form\n\n")
 
-
-
-#excel= pd.read_excel(r"C:\Users\khanh\Desktop\ContactBookAuto-main\lam hop dong.xlsx",sheet_name='29.9',header = None,dtype='object')# header = None tức là k bỏ dòng đầu ß
-#name, address, dob, idnumber, idissueddate, id issued place,  bank ac , bank name , bank address , swiftcode ,email
-#"C:\Users\khanh\Desktop\lam hop dong 30.9.xlsx"
 excel= pd.read_excel(r"C:\Users\khanh\Desktop\01.Photo_Hồ sơ freelance (06.10).xlsx",sheet_name='Contractbook Data Input',dtype='object')
 c=[0,1,2,3,4,5,6,7,8,9,10] 
 for i in excel.values:
